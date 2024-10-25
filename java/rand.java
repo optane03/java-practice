9236aa08224c2e652b6357e5723ae9d09ce7d3be
optane03 <161130811+optane03@users.noreply.github.com>
2024-10-25 18:32:49 +0530

    Delete java directory

--- java/rand.java
+++
@@ -1,8 +0,0 @@
-import java.util.Random;
-public class rand {
-   public static void main(String[] args) {
-    Random r =new Random();
-    int a= r.nextInt(3);
-    System.out.println(+a);
-   }
-}
