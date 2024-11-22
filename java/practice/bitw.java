59df47b059b6da21ffc4969169024485236eb7e3
optane03 <161130811+optane03@users.noreply.github.com>
2024-11-22 19:51:14 +0530

    Add files via upload

---
+++ java/practice/bitw.java
@@ -0,0 +1,11 @@
+class bitw{
+    public static void main(String[] args) {
+        int a=7;
+        int b=6;
+
+        System.out.println("and: "+(a&b));
+        System.out.println("or: "+(a|b));
+        System.out.println("xor: "+(a^b));
+        System.out.println("not: "+ ~a);
+    }
+}
