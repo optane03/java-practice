ef65b5d0f41fc32134b0e6502947be888b445a90
optane03 <161130811+optane03@users.noreply.github.com>
2024-11-22 19:50:36 +0530

    Add files via upload

---
+++ java/String/sublen.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class sublen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int len = 0;
+        int co = 0;
+        for (int i = 0; i < s.length(); i++) {
+            co++;
+            if(s.charAt(i)=='.'){
+                if(len < co){
+                    len = co;
+                }
+                co = 0;
+            }
+        }
+        System.out.println(len);
+    }
+}
